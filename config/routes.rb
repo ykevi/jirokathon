49ca9c67430ccbfe772e3d95a5a543260ecc1cf5
tarousinpo <shinpo@miraie-group.jp>
2018-06-09 17:50:00 +0900

    投稿フォーム作成’

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'top#index'
+  resources :user_donations
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
